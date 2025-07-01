2e56d38834611b3f4c9bf853496efcac1b990bef
Visal Suwanarat <visal@amitysolutions.com>
2025-07-01 17:19:23 +0700

    feat: implement splash cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@
 import Chatbox from "@/components/Chatbox";
 import Hero from "@/components/Hero";
 import { TopicProvider } from "@/context/TopicContext";
-
+import SplashCursor from "../components/SplashCursor";
 export default function Home() {
   return (
     <TopicProvider>
       <div className="min-h-screen flex  flex-col items-center justify-center">
+        <SplashCursor />
         <Hero />
         <Chatbox />
       </div>
